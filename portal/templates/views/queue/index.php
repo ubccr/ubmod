d4a521e1ce4a5921bf43ca75fe2f594fe7d2b4c8
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 16:10:28 +0000

    Improved grammar

--- portal/templates/views/queue/index.php
+++ portal/templates/views/queue/index.php
@@ -16,5 +16,5 @@ Ext.onReady(function(){
 <div id="stats"></div>
 <br/>
 <div class="chart-desc">
-This table provides detailed information on machine queues, including average job size, average wait time, and average run time.   Clicking once on the headings in each of the columns will sort the column (Table) from high to low. A second click will inverse the sort.   The Search capability allows you to search for a particular queue.
+This table provides detailed information on machine queues, including average job size, average wait time, and average run time.   Clicking once on the headings in each of the columns will sort the column (Table) from high to low. A second click will reverse the sort.   The Search capability allows you to search for a particular queue.
 </div>
