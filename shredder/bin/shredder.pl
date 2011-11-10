bfff4e2eddbcffd78f0527dd813484c3eee829bf
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 15:56:56 +0000

    PBS shredder implementation

---
+++ shredder/bin/shredder.pl
@@ -0,0 +1,212 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+use DBI;
+use Getopt::Long;
+use Config::Tiny;
+use Ubmod::Shredder::Pbs;
+
+my $Dbh;
+
+sub main {
+    my ( $file, $dir, $host, $shred, $update, $verbose, $help );
+
+    my $result = GetOptions(
+        'in|i=s'    => \$file,
+        'dir|d=s'   => \$dir,
+        'host|H=s'  => \$host,
+        'shred|s'   => \$shred,
+        'update|u'  => \$update,
+        'verbose|v' => \$verbose,
+        'help|h'    => \$help,
+    );
+
+    if ($help) {
+        print usage();
+        exit 0;
+    }
+
+    my $config = Config::Tiny->read("$FindBin::Bin/../config/settings.ini");
+    db_connect(@{ $config->{database} }{qw{ dsn user password }});
+
+    if ($shred) {
+        my $shredder = Ubmod::Shredder::Pbs->new();
+
+        $shredder->set_host($host) if $host;
+
+        if ($dir) {
+            process_dir($shredder, $dir);
+        } elsif ($file) {
+            process_file($shredder, $file);
+        }
+    } elsif ($update) {
+        # TODO
+    } else {
+        die usage();
+    }
+}
+
+sub usage {
+    return <<"EOT";
+NAME
+    $0 - shred accounting log files
+
+SYNOPSIS
+    $0 [-u] [-s] [-H host] [-i file|-d dir]
+
+OPTIONS
+    -i, --in
+        input file
+
+    -d, --dir
+        location of accounting log directory
+
+    -s, --shred
+        shred accounting file(s)
+
+    -u, --update
+        update aggregate tables
+
+    -H, --host hostname
+        explicity set host from which the log file(s) originated from
+
+    -v, --verbose
+        verbose output
+
+    -h, --help
+        display this text and exit
+
+EXAMPLES
+    $0 -v -s -H your.host.org -d /var/spool/pbs/server_priv/accounting
+
+    $0 -v -u
+EOT
+}
+
+sub process_directory {
+    my ( $shredder, $dir ) = @_;
+}
+
+sub process_file {
+    my ( $shredder, $file ) = @_;
+
+    open my ($fh), '<', $file or die "Could not open file '$file': $!";
+
+    $shredder->set_fh($fh);
+
+    while (my $event = $shredder->shred()) {
+        #use Data::Dumper;
+        #print Dumper($event);
+
+        insert_event($event);
+    }
+}
+
+sub db_connect {
+    my ( $dsn, $user, $pass ) = @_;
+    $Dbh = DBI->connect( $dsn, $user, $pass );
+}
+
+sub insert_event {
+    my ($event) = @_;
+    my $sth = $Dbh->prepare(q{
+        INSERT INTO event SET
+            date_key = ?,
+            job_id = ?,
+            job_array_index = ?,
+            host = LOWER(?),
+            type = ?,
+            user = LOWER(?),
+            ugroup = LOWER(?),
+            queue = LOWER(?),
+            ctime = FROM_UNIXTIME(?),
+            qtime = FROM_UNIXTIME(?),
+            start = FROM_UNIXTIME(?),
+            end = FROM_UNIXTIME(?),
+            etime = FROM_UNIXTIME(?),
+            exit_status = ?,
+            session = ?,
+            requestor = ?,
+            jobname = ?,
+            account = ?,
+            exec_host = ?,
+            resources_used_vmem = ?,
+            resources_used_mem = ?,
+            resources_used_walltime = ?,
+            resources_used_nodes = ?,
+            resources_used_cpus = ?,
+            resources_used_cput = ?,
+            resource_list_nodes = ?,
+            resource_list_procs = ?,
+            resource_list_neednodes = ?,
+            resource_list_pcput = ?,
+            resource_list_cput = ?,
+            resource_list_walltime = ?,
+            resource_list_ncpus = ?,
+            resource_list_nodect = ?,
+            resource_list_mem = ?,
+            resource_list_pmem = ?
+    });
+    $sth->execute(@$event{qw{
+        date_key
+        job_id
+        job_array_index
+        host
+        type
+        user
+        group
+        queue
+        ctime
+        qtime
+        start
+        end
+        etime
+        exit_status
+        session
+        requestor
+        jobname
+        account
+        exec_host
+        resources_used_vmem
+        resources_used_mem
+        resources_used_walltime
+        resources_used_nodes
+        resources_used_cpus
+        resources_used_cput
+        resource_list_nodes
+        resource_list_procs
+        resource_list_neednodes
+        resource_list_pcput
+        resource_list_cput
+        resource_list_walltime
+        resource_list_ncpus
+        resource_list_nodect
+        resource_list_mem
+        resource_list_pmem
+    }});
+
+    return $Dbh->{mysql_insertid};
+}
+
+sub insert_host_log {
+    my ($log) = @_;
+    my $sth = $Dbh->prepare(q{
+        INSERT INTO host_log SET
+            event_id = ?,
+            host = ?,
+            cpu = ?
+    });
+    $sth->execute(@$log{qw{ event_id host cpu }});
+}
+
+sub get_event_max_date {
+    my $sth = $Dbh->prepare(q{
+        SELECT MAX(date_key) FROM event
+    });
+    $sth->execute();
+    return $sth->fetchrow_arrayref->[0];
+}
+
+main(@ARGV) unless caller();
---
+++ shredder/lib/Ubmod/Shredder/Pbs.pm
@@ -0,0 +1,192 @@
+package Ubmod::Shredder::Pbs;
+use strict;
+use warnings;
+
+my $pattern = qr|
+    ^
+    (\d{2}/\d{2}/\d{4}\s\d{2}:\d{2}:\d{2})    # Date and time
+    ;
+    (\w)                                      # Event type
+    ;
+    ([^;]+)                                   # Job ID
+    ;
+    (.*)                                      # Params
+|x;
+
+my %params = (
+    user                    => '',
+    group                   => '',
+    queue                   => '',
+    ctime                   => '',
+    qtime                   => '',
+    start                   => '',
+    end                     => '',
+    etime                   => '',
+    exit_status             => '',
+    session                 => '',
+    requestor               => '',
+    jobname                 => '',
+    account                 => '',
+    exec_host               => '',
+    resources_used_vmem     => 'memory',
+    resources_used_mem      => 'memory',
+    resources_used_walltime => 'time',
+    resources_used_cput     => 'time',
+    resource_list_pcput     => 'time',
+    resource_list_cput      => 'time',
+    resource_list_walltime  => 'time',
+    resource_list_nodes     => '',
+    resource_list_procs     => '',
+    resource_list_neednodes => '',
+    resource_list_ncpus     => '',
+    resource_list_nodect    => '',
+    resource_list_mem       => 'memory',
+    resource_list_pmem      => 'memory',
+);
+
+sub new {
+    my ($class) = @_;
+    my $self = {};
+    return bless $self, $class;
+}
+
+sub set_fh {
+    my ( $self, $fh ) = @_;
+    $self->{fh} = $fh;
+}
+
+sub set_host {
+    my ( $self, $host ) = @_;
+    $self->{host} = $host;
+}
+
+sub shred {
+    my ($self) = @_;
+
+    my $line = readline($self->{fh});
+
+    return unless defined $line;
+
+    if ($line !~ $pattern) {
+        die "Malformed PBS acct line: $line\n";
+    }
+
+    my ($date, $type, $job_id, $params) = ($1, $2, $3, $4);
+
+    $date =~ s#^(\d{2})/(\d{2})/(\d{4})#$3-$1-$2#;
+
+    my $event = {
+        date_key => $date,
+        type     => $type,
+        host     => $self->{host},
+    };
+
+    $self->set_job_id_and_host( $event, $job_id );
+
+    my @parts = split /\s+/, $params;
+
+    foreach my $part (@parts) {
+        my ($key, $value) = split /=/, $part, 2;
+
+        $key =~ s/\./_/g;
+        $key = lc $key;
+
+        if ( $key eq 'exec_host' ) {
+            $self->set_exec_host($event, $value);
+        }
+
+        if (defined( my $type = $params{$key} )) {
+            if ( $type ne '' ) {
+                my $parser = "parse_$type";
+                $value = $self->$parser($value);
+            }
+            $event->{$key} = $value;
+        }
+    }
+
+    return $event;
+}
+
+sub parse_time {
+    my ( $self, $time ) = @_;
+
+    my ($h, $m, $s) = split /:/, $time;
+    return $h * 60 * 60 + $m * 60 + $s;
+}
+
+sub parse_memory {
+    my ( $self, $memory ) = @_;
+
+    my $unit = 'kb';
+
+    if ($memory =~ /^\d+(.*)/) {
+        $unit = $1;
+    }
+
+    $memory =~ s/\D+//g;
+
+    return $self->scale_memory( $unit, $memory );
+}
+
+sub scale_memory {
+    my ( $self, $unit, $value ) = @_;
+
+    # XXX because our default unit is KB just return 1
+    return 1 if $unit eq 'b';
+
+    return $value if $unit eq 'kb';
+    return $value * 1024 if $unit eq 'mb';
+    return $value * 1024 * 1024 if $unit eq 'gb';
+}
+
+sub set_job_id_and_host {
+    my ( $self, $event, $job_id ) = @_;
+
+    my ( $job, $host ) = split /\./, $job_id, 2;
+
+    my ( $id, $index ) = split /-/, $job;
+
+    $event->{job_id} = $id;
+    $event->{job_array_index} = $index;
+
+    $event->{host} = $host if ! defined $event->{host};
+}
+
+sub set_exec_host {
+    my ( $self, $event, $hosts ) = @_;
+
+    my $cpus  = 0;
+    my $nodes = 0;
+
+    my %map;
+    foreach my $host (@{ $self->parse_hosts($hosts) }) {
+        $map{$host->{host}} += 1;
+    }
+
+    while (my ($host, $total) = each(%map)) {
+        $nodes++;
+        $cpus += $total;
+    }
+
+    $event->{resources_used_nodes} = $nodes;
+    $event->{resources_used_cpus} = $cpus;
+}
+
+sub parse_hosts {
+    my ( $self, $hosts ) = @_;
+
+    my @parts = split /\+/, $hosts;
+
+    my @hosts;
+    foreach my $part (@parts) {
+        my ( $host, $cpu ) = split /\//, $part;
+        push @hosts, {
+            host => $host,
+            cpu  => $cpu,
+        };
+    }
+
+    return \@hosts;
+}
+
+1;
