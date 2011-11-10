ca042e1d320ce55353e62b23dcaf83591adc6d88
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 16:04:04 +0000

    Modified asset setup script

--- portal/assets/setup.sh
+++ portal/assets/setup.sh
@@ -1,14 +1,35 @@
 #!/bin/sh
 ASSET_DIR=$(dirname $0)
+
 EXT_VERSION="4.0.2"
 EXT_ARCHIVE="ext-$EXT_VERSION-gpl.zip"
 EXT_DIR="ext-$EXT_VERSION"
 
+ZF_VERSION="1.11.6"
+ZF_ARCHIVE="ZendFramework-$ZF_VERSION.tar.gz"
+ZF_DIR="ZendFramework-$ZF_VERSION"
+
+PCHART_VERSION="2.1.1"
+PCHART_ARCHIVE="pChart$PCHART_VERSION.tar"
+PCHART_DIR="pChart$PCHART_VERSION"
+
 cd $ASSET_DIR
 
 unzip $EXT_ARCHIVE
-cp -r $EXT_DIR/ext-all.js       ../html/js
-cp -r $EXT_DIR/ext-all-debug.js ../html/js
-cp -r $EXT_DIR/resources        ../html
+cp -rf $EXT_DIR/ext-all.js ../html/js
+cp -rf $EXT_DIR/ext-all-debug.js ../html/js
+rm -rf ../html/resources
+cp -rf $EXT_DIR/resources ../html
+rm -rf $EXT_DIR
+
+tar zxvf $ZF_ARCHIVE
+rm -rf ../lib/Zend
+cp -rf $ZF_DIR/library/Zend ../lib
+rm -rf $ZF_DIR
+
+tar xvf $PCHART_ARCHIVE
+rm -rf ../lib/pChart
+cp -rf $PCHART_DIR ../lib/pChart
+rm -rf $PCHART_DIR
 
 exit 0
