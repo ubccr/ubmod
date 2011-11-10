d196b278e140cb685a3f961d1f84869f2bdbdc0d
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 16:11:49 +0000

    Refactored show/hide pie/bar charts on dashboard

--- portal/templates/views/dashboard/index.php
+++ portal/templates/views/dashboard/index.php
@@ -1,26 +1,25 @@
 <script type="text/javascript">
-var index = 1;
 function chartswap(type) {
-    var upie = Ext.get("upie");
-    if(upie != null) upie.setVisibilityMode(Element.DISPLAY);
-    var ubar = Ext.get("ubar");
-    if(ubar != null) ubar.setVisibilityMode(Element.DISPLAY);
-    var gpie = Ext.get("gpie");
-    if(gpie != null) gpie.setVisibilityMode(Element.DISPLAY);
-    var gbar = Ext.get("gbar");
-    if(gbar != null) gbar.setVisibilityMode(Element.DISPLAY);
-    if(index == 1 && type == 'bar') {
-        if(upie != null) upie.setStyle("display", "none");
-        if(gpie != null) gpie.setStyle("display", "none");
-        if(ubar != null) ubar.setStyle("display", "block");
-        if(gbar != null) gbar.setStyle("display", "block");
-        index = 0;
-    } else if(index == 0 && type == 'pie'){
-        if(ubar != null) ubar.setStyle("display", "none");
-        if(gbar != null) gbar.setStyle("display", "none");
-        if(upie != null) upie.setStyle("display", "block");
-        if(gpie != null) gpie.setStyle("display", "block");
-        index = 1;
+    var upie = Ext.get('upie'),
+        ubar = Ext.get('ubar'),
+        gpie = Ext.get('gpie'),
+        gbar = Ext.get('gbar');
+
+    if (upie !== null) { upie.setVisibilityMode(Ext.Element.DISPLAY); }
+    if (ubar !== null) { ubar.setVisibilityMode(Ext.Element.DISPLAY); }
+    if (gpie !== null) { gpie.setVisibilityMode(Ext.Element.DISPLAY); }
+    if (gbar !== null) { gbar.setVisibilityMode(Ext.Element.DISPLAY); }
+
+    if (type === 'bar') {
+        if (upie !== null) { upie.hide(); }
+        if (gpie !== null) { gpie.hide(); }
+        if (ubar !== null) { ubar.show(); }
+        if (gbar !== null) { gbar.show(); }
+    } else if (type === 'pie') {
+        if (ubar !== null) { ubar.hide(); }
+        if (gbar !== null) { gbar.hide(); }
+        if (upie !== null) { upie.show(); }
+        if (gpie !== null) { gpie.show(); }
     }
 }
 
