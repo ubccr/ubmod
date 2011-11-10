fff59ec32c124f373c96ccccd51ee9dc256a393e
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 16:23:26 +0000

    Updated about page

--- portal/templates/views/about/index.php
+++ portal/templates/views/about/index.php
@@ -5,25 +5,24 @@
   <br/>
   <p>UBMoD is an open source data warehouse and web portal for mining
   statistical data from resource managers
-  (<a href="http://www.clusterresources.com/pages/products/torque-resource-manager.php">Torque</a>
-  and <a href="http://www.openpbs.org/">OpenPBS</a> currently supported)
-  commonly found in high-performance computing environments. UBMoD presents
-  resource utilization (CPU cycles consumed, total jobs, avg wait time, etc)
-  for individual users, research groups, departments, and decanal units. A
-  role-based authentication scheme is used to display data in the format best
-  suited to the end-user (for example, as a student, research group leader,
-  department chair, dean, etc.). The information can be presented in easy to
-  understand interactive charts, graphs, and tables, with the added ability to
-  display finer levels of detail. UBMoD is written in Java and uses MySQL as
-  the backend database. The web portal runs inside a servlet container
-  (Tomcat, Jetty) and can be viewed using any popular modern web browser that
-  supports Javascript.
+  (<a href="http://www.clusterresources.com/pages/products/torque-resource-manager.php">Torque</a>,
+  <a href="http://www.openpbs.org/">OpenPBS</a> and
+  <a href="http://gridscheduler.sourceforge.net/">Sun Grid Engine</a>
+  currently supported) commonly found in high-performance computing
+  environments. UBMoD presents resource utilization (CPU cycles consumed,
+  total jobs, avg wait time, etc) for individual users, and groups.  The
+  information can be presented in easy to understand charts, graphs, and
+  tables, with the added ability to display finer levels of detail. The UBMoD
+  frontend is written in PHP. It uses MySQL as the backend database and uses
+  Perl for parsing resource manager accounting files. The web portal runs on
+  the Apache HTTP server and can be viewed using any popular modern web
+  browser that supports Javascript.
   </p>
   <br/>
-  <p>UBmod was developed by the <a href="http://www.ccr.buffalo.edu">Center
-  for Computational Research</a> at the
-  <a href="http://www.buffalo.edu">University at Buffalo</a>, SUNY and is
-  distributed under the
+  <p>UBmod was developed by the
+  <a href="http://www.ccr.buffalo.edu">Center for Computational Research</a>
+  at the <a href="http://www.buffalo.edu">University at Buffalo</a>, SUNY and
+  is distributed under the
   <a href="http://www.ccr.buffalo.edu/licenses/ubpl.txt">University at Buffalo
   Public License ("UBPL") Version 1.0</a>. Further details as well as the
   source code can be found at
