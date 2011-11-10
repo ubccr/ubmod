d5e04b86105c4e923ac62fa45127c810a8a77c28
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 16:34:17 +0000

    Updated tag report descriptions

--- portal/templates/views/tag/keys.php
+++ portal/templates/views/tag/keys.php
@@ -9,11 +9,15 @@ Ext.onReady(function () {
   These plots provide a quick snapshot of utilization group by tag values for
   the selected tag key. Data is presented in either Pie or Bar chart format.
   In the Pie chart format, the utilization is given as a percentage of total
-  CPU days consumed and in the Bar chart format in total CPU days consumed.
+  days and in the Bar chart format in total days. The "Other" slice in the pie
+  chart represents the sum of any tag keys that are not shown and the
+  remaining wall time not associated with any key. If the tag keys are not
+  mutually exclusive, this slice (as well as the percentages) may not be
+  accurate with respect to the total amount of wall time used.
 </div>
 <div class="chart-desc">
   If the selected time period spans multiple months, stacked area charts are
-  displayed with the total CPU days consumed for each month that is included
+  displayed with the total wall time in days for each month that is included
   in the time period.
 </div>
 <div class="chart-desc">
