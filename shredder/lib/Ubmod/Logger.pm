471aae141e368ce79fa4070df01da67bf08b82db
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2011-11-10 15:59:08 +0000

    Added documentation

--- shredder/lib/Ubmod/Logger.pm
+++ shredder/lib/Ubmod/Logger.pm
@@ -30,3 +30,92 @@ sub fatal {
 }
 
 1;
+
+
+__END__
+
+=head1 NAME
+
+Ubmod::Logger - UBMoD logging module
+
+=head1 SYNOPSIS
+
+    my $logger = Ubmod::Logger->new($verbose);
+    $logger->info('Message.');
+    $logger->warn('Warning!');
+    $logger->fatal('Fatal Error!!!');
+
+=head1 DESCRIPTION
+
+This module implements a simple logging mechanism (printing to STOUT).
+
+=head1 CONSTRUCTOR
+
+=head2 new()
+
+=head2 new($verbose)
+
+    my $logger = Ubmod::Logger->new($verbose);
+
+Use a true value for $verbose to indicate that all messages should be
+sent to standard out.  Otherwise, only C<warn> and C<fatal> messages
+will be output.
+
+=head1 METHODS
+
+=head2 info()
+
+    $logger->info('Message.');
+
+Log a message.  The message is only output when the
+verbose option is set.
+
+=head2 warn()
+
+    $logger->warn('Warning!');
+
+Log a warning.
+
+=head2 fatal()
+
+    $logger->fatal('Fatal Error!!!');
+
+Log a fatal error.
+
+=head1 AUTHOR
+
+Jeffrey T. Palmer <jtpalmer@ccr.buffalo.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+The contents of this file are subject to the University at Buffalo Public
+License Version 1.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+http://www.ccr.buffalo.edu/licenses/ubpl.txt
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is UBMoD.
+
+The Initial Developer of the Original Code is Research Foundation of State
+University of New York, on behalf of University at Buffalo.
+
+Portions created by the Initial Developer are Copyright (C) 2007 Research
+Foundation of State University of New York, on behalf of University at
+Buffalo.  All Rights Reserved.
+
+Alternatively, the contents of this file may be used under the terms of
+either the GNU General Public License Version 2 (the "GPL"), or the GNU
+Lesser General Public License Version 2.1 (the "LGPL"), in which case the
+provisions of the GPL or the LGPL are applicable instead of those above. If
+you wish to allow use of your version of this file only under the terms of
+either the GPL or the LGPL, and not to allow others to use your version of
+this file under the terms of the UBPL, indicate your decision by deleting
+the provisions above and replace them with the notice and other provisions
+required by the GPL or the LGPL. If you do not delete the provisions above,
+a recipient may use your version of this file under the terms of any one of
+the UBPL, the GPL or the LGPL.
+
+=cut
--- shredder/lib/Ubmod/Shredder/Pbs.pm
+++ shredder/lib/Ubmod/Shredder/Pbs.pm
@@ -195,3 +195,86 @@ sub parse_hosts {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Ubmod::Shredder::Pbs - Parse PBS/TORQUE format accounting logs
+
+=head1 SYNOPSIS
+
+    my $shredder = Ubmod::Shredder::Pbs->new( logger => $Logger );
+    $shredder->set_host($host);
+    $shredder->set_fh($fh);
+    my $event = $shredder->shred();
+
+=head1 DESCRIPTION
+
+This module uses data in the C<event> table to produce aggregate data,
+which can then be viewed using the UBMoD portal.
+
+=head1 CONSTRUCTOR
+
+=head2 new( logger => $logger );
+
+    my $aggregator = Ubmod::Shredder::Pbs->new( logger => $logger );
+
+$logger is required, it should be an instance of Ubmod::Logger.
+
+=head1 METHODS
+
+=head2 set_host($host)
+
+Set the name of the cluster to C<$host>.  This will override any host
+names that are found in the log file during the shredding process.
+
+=head2 set_fh($fh)
+
+Set the current file handle to C<$fh>.  $fh should be a file handle to
+an accounting log file that has been opened for read access.
+
+=head2 shred()
+
+Shred a single line in the current file.  Returns C<undef> when there
+are no more lines to be processed.  Returns an empty hashref if there
+was an error.  Otherwise, returns a hashref containing the information
+parsed from the current line.
+
+=head1 AUTHOR
+
+Jeffrey T. Palmer <jtpalmer@ccr.buffalo.edu>
+
+=head1 COPYRIGHT AND LICENSE
+
+The contents of this file are subject to the University at Buffalo Public
+License Version 1.0 (the "License"); you may not use this file except in
+compliance with the License. You may obtain a copy of the License at
+http://www.ccr.buffalo.edu/licenses/ubpl.txt
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+the specific language governing rights and limitations under the License.
+
+The Original Code is UBMoD.
+
+The Initial Developer of the Original Code is Research Foundation of State
+University of New York, on behalf of University at Buffalo.
+
+Portions created by the Initial Developer are Copyright (C) 2007 Research
+Foundation of State University of New York, on behalf of University at
+Buffalo.  All Rights Reserved.
+
+Alternatively, the contents of this file may be used under the terms of
+either the GNU General Public License Version 2 (the "GPL"), or the GNU
+Lesser General Public License Version 2.1 (the "LGPL"), in which case the
+provisions of the GPL or the LGPL are applicable instead of those above. If
+you wish to allow use of your version of this file only under the terms of
+either the GPL or the LGPL, and not to allow others to use your version of
+this file under the terms of the UBPL, indicate your decision by deleting
+the provisions above and replace them with the notice and other provisions
+required by the GPL or the LGPL. If you do not delete the provisions above,
+a recipient may use your version of this file under the terms of any one of
+the UBPL, the GPL or the LGPL.
+
+=cut
