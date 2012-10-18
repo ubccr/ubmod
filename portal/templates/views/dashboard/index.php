7d1ee10f48c60d82fa1978fe13d21df672c1e76e
jtpalmer <jtpalmer@431b86ff-963b-0410-84b2-bb6f1fb6fb75>
2012-10-18 18:39:18 +0000

    Changed dashboard for users without group access

--- portal/templates/views/dashboard/index.php
+++ portal/templates/views/dashboard/index.php
@@ -6,13 +6,23 @@
           url: Ubmod.baseUrl + '/dashboard/utilization'
       });
   });
-<?php else: ?>
+<?php elseif ($request->isAllowed('group', 'details')): ?>
   Ext.onReady(function () {
       Ubmod.app.createPartial({
           renderTo: 'dash-chart',
           url: Ubmod.baseUrl + '/group/details',
           params: {
               group_id: <?php echo $request->getGroupId(); ?>
+          }
+      });
+  });
+<?php else: ?>
+  Ext.onReady(function () {
+      Ubmod.app.createPartial({
+          renderTo: 'dash-chart',
+          url: Ubmod.baseUrl + '/user/details',
+          params: {
+              user_id: <?php echo $request->getUserId(); ?>
           }
       });
   });
